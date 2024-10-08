dc76c7d54f8842cf43b19421b87a9e4e0ac2de78
yuxinwen <teacheryuyxw@outlook.com>
2024-10-08 18:08:11 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Prism.Ioc;
+
+namespace MyToDoList
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+        protected override void RegisterTypes( IContainerRegistry registry)
+        {
+        }
+    }
+
+}
